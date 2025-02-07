316b1cc16f809db0f9216206a6acb7bd4ab6907a
web-tomson <tommyansu002@gmail.com>
2025-02-07 14:40:36 +0000

    ww5

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-document.addEventListener("DOMContentLoaded", () => {
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+  document.addEventListener("DOMContentLoaded", () => {
     // Update footer dates
     const currentYear = new Date().getFullYear();
     const lastModified = document.lastModified;
-
+  
     document.getElementById("currentYear").textContent = currentYear;
     document.getElementById("lastModified").textContent = lastModified;
-
+  
     // Calculate and display wind chill
     const temperature = 10; // °C
     const windSpeed = 5; // km/h
-
+  
     const calculateWindChill = (temp, wind) => {
         if (temp <= 10 && wind > 4.8) {
             return (
@@ -21,7 +49,8 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         return "N/A";
     };
-
+  
     const windChill = calculateWindChill(temperature, windSpeed);
     document.getElementById("windChill").textContent = windChill;
-});
+  });
+  
